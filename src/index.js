832c6910fbd8a1b2a4737d994428f318ccc98c89
Zenonist <phonarnun@hotmail.com>
2023-01-30 23:19:10 +0000

    feat: declare a winner

--- src/index.js
+++ src/index.js
@@ -9,78 +9,108 @@ function Square(props) {
         </button>
     )
 }
-  
-  class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-    handleClick(i) {
-        // * Set 'X' in each square array instead of setting 'X' in each square
-        // slice() creates a copy of the squares array to modify instead of modifying the existing array
-        // * Immutability is important, because it helps you build pure components in React
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          squares: squares,
-          // * Flip the value of xIsNext (X -> O or O -> X)
-          xIsNext: !this.state.xIsNext,
-        });
-    }
-    renderSquare(i) {
-      return <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />;
-    }
-  
-    render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // * Check that every square in the line has the same value -> return winner (X or O)
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
+  return null;
+}
+
+
+class Board extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          squares: Array(9).fill(null),
+          xIsNext: true,
+      };
   }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
-  
+  handleClick(i) {
+      // * Set 'X' in each square array instead of setting 'X' in each square
+      // slice() creates a copy of the squares array to modify instead of modifying the existing array
+      // * Immutability is important, because it helps you build pure components in React
+      const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        // * Flip the value of xIsNext (X -> O or O -> X)
+        xIsNext: !this.state.xIsNext,
+      });
+  }
+  renderSquare(i) {
+    return <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
